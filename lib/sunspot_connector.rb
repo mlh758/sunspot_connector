573ecd657ac6399ea62a1210b69853d8e35e46ff
Mike Harris <mike.harris@cerner.com>
2021-02-16 15:56:04 -0600

    accidentally wiped it all out for a second

--- lib/sunspot_connector.rb
+++ lib/sunspot_connector.rb
@@ -1,4 +1,7 @@
 require "sunspot_connector/version"
+require "sunspot_connector/config"
+require "sunspot_connector/connection"
+require "sunspot_connector/railtie"
 
 module SunspotConnector
   class Error < StandardError; end
